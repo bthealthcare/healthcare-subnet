6a6b5dc184edf2c04d0fd06084e7dc3a58fc3aac
cogent-demon <134632703+cogent-demon@users.noreply.github.com>
2024-02-03 06:31:09 -0500

    Update extracter.py

--- healthcare/dataset/extracter.py
+++ healthcare/dataset/extracter.py
@@ -1,14 +1,13 @@
 import tarfile
 import os
-
 import bittensor as bt
-from constant import Constant
+from constants import BASE_DIR
 
 # List of your .tar.gz files
 tar_files = ['images.tar.gz']
 
 # Directory where you want to extract files
-parent_dir = Constant.BASE_DIR + '/healthcare/dataset/'
+parent_dir = BASE_DIR + '/healthcare/dataset/'
 extract_to_dir = parent_dir + "validator"
 os.makedirs(extract_to_dir, exist_ok=True)
 
